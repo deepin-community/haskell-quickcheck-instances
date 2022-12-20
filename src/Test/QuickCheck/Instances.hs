6f312f13c81591a778dd6569bc5aadd62f53ffbf
Deepin Developer <deepin-dev@deepin.org>
2022-12-20 14:33:11 +0800

    feat: Init commit
    init haskell-quickcheck-instances
    
    Log:

---
+++ src/Test/QuickCheck/Instances.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE CPP              #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+{-|
+Instances are provided for the types in the packages:
+
+ * array
+
+ * bytestring
+
+ * case-insensitive
+
+ * containers
+
+ * old-time
+
+ * text
+
+ * time
+
+ * unordered-containers
+
+Since all of these instances are provided as orphans, I recommend that
+you do not use this library within another library module, so that you
+don't impose these instances on down-stream consumers of your code.
+
+For information on writing a test-suite with Cabal see
+<http://www.haskell.org/cabal/users-guide/#test-suites>
+-}
+module Test.QuickCheck.Instances () where
+
+import Test.QuickCheck.Instances.Array ()
+import Test.QuickCheck.Instances.ByteString ()
+import Test.QuickCheck.Instances.CaseInsensitive ()
+import Test.QuickCheck.Instances.Containers ()
+import Test.QuickCheck.Instances.Hashable ()
+import Test.QuickCheck.Instances.Natural ()
+import Test.QuickCheck.Instances.Natural ()
+import Test.QuickCheck.Instances.OldTime ()
+import Test.QuickCheck.Instances.Scientific ()
+import Test.QuickCheck.Instances.Semigroup ()
+import Test.QuickCheck.Instances.Tagged ()
+import Test.QuickCheck.Instances.Text ()
+import Test.QuickCheck.Instances.These ()
+import Test.QuickCheck.Instances.Time ()
+import Test.QuickCheck.Instances.Transformer ()
+import Test.QuickCheck.Instances.UnorderedContainers ()
+import Test.QuickCheck.Instances.UUID ()
+import Test.QuickCheck.Instances.Vector ()
+import Test.QuickCheck.Instances.Void ()
---
+++ src/Test/QuickCheck/Instances/Transformer.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE CPP              #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+module Test.QuickCheck.Instances.Transformer () where
+
+import Prelude ()
+import Prelude.Compat
+
+import Control.Monad.Trans.Maybe (MaybeT (..))
+import Data.Functor.Sum          (Sum (..))
+
+import Test.QuickCheck
+
+-------------------------------------------------------------------------------
+-- transformers
+-------------------------------------------------------------------------------
+
+-- TODO: CoArbitrary and Function, needs Coarbitrary1 and Function1
+
+instance (Arbitrary1 m) => Arbitrary1 (MaybeT m) where
+  liftArbitrary = fmap MaybeT . liftArbitrary . liftArbitrary
+  liftShrink shr (MaybeT m) = map MaybeT (liftShrink (liftShrink shr) m)
+
+instance (Arbitrary1 m, Arbitrary a) => Arbitrary (MaybeT m a) where
+  arbitrary = arbitrary1
+  shrink = shrink1
+
+instance (Arbitrary1 f, Arbitrary1 g) => Arbitrary1 (Sum f g) where
+  liftArbitrary arb = oneof [fmap InL (liftArbitrary arb), fmap InR (liftArbitrary arb)]
+  liftShrink shr (InL f) = map InL (liftShrink shr f)
+  liftShrink shr (InR g) = map InR (liftShrink shr g)
+
+instance (Arbitrary1 f, Arbitrary1 g, Arbitrary a) => Arbitrary (Sum f g a) where
+  arbitrary = arbitrary1
+  shrink = shrink1
